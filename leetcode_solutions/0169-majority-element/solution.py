db3388b4aa69323eb9ad58f0a200961397a6b2c6
emilylienhuang <eng@uccs.edu>
2024-05-30 17:39:27 +0000

    [LeetCode Sync] Runtime - 163 ms (5.34%), Memory - 17.9 MB (98.44%)

--- leetcode_solutions/0169-majority-element/solution.py
+++ leetcode_solutions/0169-majority-element/solution.py
@@ -1,13 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums.sort()
-        dict = {}
-        times = len(nums)//2
-        for value in nums:
-            if value not in dict:
-                dict[value] = 1
-            else:
-                dict[value] += 1
-            if dict[value] > times:
-                return value
-        return nums[times]
+        return nums[len(nums) // 2]
