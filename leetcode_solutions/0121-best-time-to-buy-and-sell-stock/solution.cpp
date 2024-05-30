4b977569a0feaf8c69e9b4e2bcdc52ab295fd5ae
emilylienhuang <eng@uccs.edu>
2024-05-30 17:46:09 +0000

    [LeetCode Sync] Runtime - 77 ms (5.46%), Memory - 95.8 MB (99.84%)

--- leetcode_solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ leetcode_solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX;
-        int maxProfit = 0;
-        for (auto & price : prices){
-            if (minPrice > price){
+        int minPrice = prices[0];
+        int maxProf = 0;
+        for(auto &price: prices){
+            if(price < minPrice){
                 minPrice = price;
-            } else if (price - minPrice > maxProfit){
-                maxProfit = price - minPrice;
+            } else if(price - minPrice > maxProf){
+                maxProf = price - minPrice;
             }
         }
-        return maxProfit;
+        return maxProf;
     }
 };
