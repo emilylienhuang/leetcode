ab615a11eef455e8c0cd64a6ccf23319f47642c0
emilylienhuang <eng@uccs.edu>
2024-07-01 10:15:31 +0000

    [LeetCode Sync] Runtime - 18 ms (18.52%), Memory - 11.8 MB (82.54%)

--- leetcode_solutions/0274-h-index/solution.py
+++ leetcode_solutions/0274-h-index/solution.py
@@ -1,10 +1,13 @@
-class Solution:
-    def hIndex(self, citations: List[int]) -> int:
-        citations.sort()
-        n = len(citations)
-
-        for i , curr_citation_count in enumerate(citations):
-            if n - i <= curr_citation_count:
-                return n - i
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        citations.sort(reverse=True)
+        h = 0
+        for i, cite in enumerate(citations):
+                if cite >= i + 1:
+                    h+=1
+        return h
         
-        return 0
