a4f7221a142f56e4c5d0620e70f67b5f208c145d
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2024-12-06 16:37:23 -0700

    Time: 423 ms (11.15%), Space: 13.1 MB (11.17%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -5,24 +5,7 @@ class Solution(object):
         :type h: int
         :rtype: int
         """
-        '''
-        edge cases: only one pile
-        min_num_bananas (l), max_num_bananas(r)
-        time = 0
-        while l <= r:
-            k = l + r // 2
-            hours = 0
-            for pile in piles:
-                hours += chomp down on pile
-            if hours <= h:
-                time = min(hours, time)
-                r = k - 1
-            else:
-                # we need to eat more bananas
-                l = k + 1
-        '''
-
-        l, r = 1, max(piles)
+        l, r = 1, max(piles) #  = [1, 2, 3, ..... , max(piles)]
         optimal_k = max(piles)
         while l <= r:
             k = (l + r)//2
