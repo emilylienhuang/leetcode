7675ff088d8b0e8a0506f05d8d94bb206c04519a
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2024-12-20 16:05:09 -0700

    Time: 11 ms (54.59%), Space: 14.5 MB (5.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # Moore's algorithm
+        # O(1) space
+        # O(n) time
         return self.findCandidate(nums)
         
         
@@ -13,8 +16,12 @@ class Solution(object):
         count = 1 # keep track of the number of occurences
         for i in range(1, len(nums)):
             if nums[i] != nums[i_me]:
+                # cancel out one of the majority elements
                 count -= 1
+            else:
+                # increment the majority element count 
+                count += 1
             if count == 0:
                 i_me = i
-            count += 1
+                count = 1
         return nums[i_me]
