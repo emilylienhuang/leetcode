1a37017852f405134e0c17a1643e32d1c3bf8ceb
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-03 10:57:45 -0700

    Time: 2581 ms (19.73%), Space: 180.1 MB (9.16%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,43 +1,30 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        '''
-        Problem Understanding: 
-        1. we can think of this as a fully connected graph where each node is connected to each other node with a weight of the manhattan distance
-        2. We can map the points to each other using an adjacency matrix where each entry is the manhattan distance between the two points
-        3. We can select the minimum entries in each row if not already in visited
-        '''
+        N = len(points)
+        adj_mat = {i: [] for i in range(N)}
+        for i in range(N):
+            x1, y1 = points[i]
+            for j in range(i + 1, N):
+                # O(n^2) time - iterating over each set of node pairs
+                # O(n^2) space - each connection filed twice
+                x2,y2 = points[j]
+                m_dist = abs(x1 - x2) + abs(y1 - y2)
+                adj_mat[i].append([m_dist, j])
+                adj_mat[j].append([m_dist, i])
 
-        # keep track of the index in the adjacency matrix
-        point_to_index = {}
-        for i, point in enumerate(points):
-            point_to_index[i] = point
-        
-        # keep track of the manhattan distance in the matrix
-        adj_mat = [[float('inf')] * len(points) for _ in range(len(points))]
-        
-        # iterate over each point to point pair in the adjacency matrix
-        for r in range(len(points)):
-            for c in range(len(points)):
-                if r == c:
-                    # manhattan distance between a point and itself is 0
-                    continue
-                else:
-                    # get the x,y of each respective point
-                    xr, yr = point_to_index[r]
-                    xc, yc = point_to_index[c]
-                    m_dist = abs(xr - xc) + abs(yr - yc)
-                    adj_mat[r][c] = m_dist
-        
-        min_dist = 0
-        for c in range(1, len(points)):
-            min_cost = float('inf')
-            for r in range(c - 1, len(points)):
-                min_cost = min(min_cost, adj_mat[r][c])
-            min_dist += min_cost
-        print(min_dist)
-        return min_dist
-        
-        
+        # Now we use prim's algorithm to create an MST
+        minHeap = [[0,0]] # cost, node
+        visited = set() # check if a node has been visited
+        distance = 0
+        while len(visited) < N: # O(n) time
+            cost, node = heapq.heappop(minHeap) # O(logn)
+            if node in visited:
+                continue
+            distance += cost
+            visited.add(node)
+            for neighCost, neigh in adj_mat[node]: # O(n)
+                if neigh not in visited:
+                    heapq.heappush(minHeap, [neighCost, neigh]) # O(log n)
+        return distance
 
 
-                
