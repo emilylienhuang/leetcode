e5ea6469ca38572f8b8ea8f89de4aec00c3b447b
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-03 12:15:50 -0700

    Time: 0 ms (100%), Space: 18.7 MB (23.42%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        num_set = set(nums)
         n = len(nums)
-        for i in range(n + 1):
-            if i not in num_set:
-                return i
+        xorr = n
+        for i in range(len(nums)):
+            xorr ^= i ^ nums[i]
+        return xorr
