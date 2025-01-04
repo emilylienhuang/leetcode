3994249037b4a1e0414f440909cffa06f8f2b845
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-04 14:17:47 -0700

    Time: 13 ms (37.94%), Space: 17.9 MB (12.67%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
         res = []
-
-        def make_combinations(idx, comb, total):
+        def backtrack(idx, comb, total):
             if total == target:
                 res.append(comb[:])
                 return
             if total > target or idx >= len(candidates):
-                return 
+                return
             
             comb.append(candidates[idx])
-            make_combinations(idx, comb, total+candidates[idx])
+            backtrack(idx, comb, total + candidates[idx])
             comb.pop()
-            make_combinations(idx+1, comb, total)
+            backtrack(idx + 1, comb, total)
             return res
-        return make_combinations(0,[], 0)
+        
+        return backtrack(0, [], 0)
