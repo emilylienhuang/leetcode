72d64f185e65ceb80c33528a24a98181141f2986
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-04 13:31:03 -0700

    Time: 128 ms (76.47%), Space: 17.9 MB (66.84%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,17 +1,10 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        '''
-        This is a DP algorithm checking each way you can make change based on a given amount of coins
-        dp[0] = 0
-        iterate up to n using i
-            for coin in coins
-                if dp[i] == coin : dp[i] += 1
-                dp[i]  += dp[coin - i]
-
-        '''
-        dp = [0] * (amount + 1) # account for zero based indexing
+        dp = [0] * (amount + 1)
         dp[0] = 1
+
         for coin in coins:
             for i in range(coin, amount + 1):
-                dp[i] += dp[i-coin]
+                change = i - coin
+                dp[i] += dp[change]
         return dp[-1]
