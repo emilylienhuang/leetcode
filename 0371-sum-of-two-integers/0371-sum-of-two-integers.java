53155b932245f21a3a6554fd3ab2e3e6a4bd8f91
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-05 11:24:39 -0700

    Time: 0 ms (100%), Space: 40.1 MB (82.05%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -2,7 +2,7 @@ class Solution {
     public int getSum(int a, int b) {
         int tmp;
         while (b != 0){
-            tmp = (a & b) << 1;
+            tmp = (a & b) << 1; // Calculate carry over
             a = a ^ b;
             b = tmp;
         }
