a5f89104aafaea641f9a888ddbbefbbf721c69ae
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-05 11:16:12 -0700

    Time: 43 ms (20.47%), Space: 17.8 MB (16.56%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -2,6 +2,6 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
         for i in range(32):
-           bit = (n >> i) & 1 # get the i'th bit and and it with 1 to get its value
-           res = res  | (bit << (31 - i)) # pipe into res shifting over to the furthest available
+            bit_val = (n >> i) & 1 # get if a 1 or a zero
+            res = res | bit_val << (31 - i)
         return res
