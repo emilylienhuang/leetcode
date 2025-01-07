02014a53b289c868c4e45ed44b5b7c3e7638e1ab
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-07 11:28:44 -0700

    Time: 4 ms (26.97%), Space: 11.9 MB (75.39%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) == 1:
+            return [nums]
+        
+        res = []
+        def backtrack(start):
+            if start == len(nums):
+                res.append(nums[:])
+                return
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+        backtrack(0)
+        return res
