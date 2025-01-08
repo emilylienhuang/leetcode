6b7f0da0141fc75941b0e747b9964e4b82130fad
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-08 11:09:23 -0700

    Time: 0 ms (100%), Space: 17.8 MB (14.48%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -3,28 +3,28 @@ class Solution:
         if not digits:
             return []
         
-        # O(n) space allocation to save the keyboard
         digits_to_letters = {
             '2': 'abc',
             '3': 'def',
             '4': 'ghi',
             '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
+            '6': 'mno', 
+            '7':'pqrs',
+            '8':'tuv',
             '9': 'wxyz'
         }
 
         res = []
-        def backtrack(comb, idx):
-            if len(comb) == len(digits):
-                res.append(comb[:])
-                return
+        def backtrack(idx, comb):
             if idx >= len(digits):
+                string = ''.join(comb[:])
+                res.append(string)
                 return
-
+            
             for letter in digits_to_letters[digits[idx]]:
-                backtrack(comb + letter, idx + 1)
-            return
-        backtrack("", 0)
+                comb.append(letter)
+                backtrack(idx + 1, comb)
+                comb.pop()
+        backtrack(0, [])
         return res
+
