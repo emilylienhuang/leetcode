df8f9a04ebf97bcd4f46bcb25120773d98d09c43
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-09 12:36:47 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,12 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        def getMoney(arr):
-            prev_rob, max_rob = 0,0
-            for num in arr:
-                tmp = max(prev_rob + num, max_rob)
-                prev_rob = max_rob
-                max_rob = tmp
-            return max_rob
-        
-        return max([getMoney(nums[1:]), getMoney(nums[:-1]), nums[0]])
+        def house_rob(arr):
+            n = len(arr)
+            dp = [-1] * (n)
+            dp[0] = arr[0]
+            dp[1] = max(arr[1], arr[0])
+            for i in range(2, n):
+                dp[i] = max(dp[i-1], dp[i-2] + arr[i])
+            return dp[n -1]
+
+        if len(nums) == 1:
+            return nums[0]
+        return max(house_rob(nums[:-1]), house_rob(nums[1:]))
