aec9b4e9bab01c420c44b6953571a7f90188b26d
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-15 15:59:12 -0700

    Time: 40 ms (42.94%), Space: 17.6 MB (42.77%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -2,6 +2,6 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
         for i in range(32):
-            bit_val = (n >> i) & 1 # get if a 1 or a zero
-            res = res | bit_val << (31 - i)
+            bit_val = (n >> i) & 1
+            res = res | (bit_val << (31 - i)) 
         return res
