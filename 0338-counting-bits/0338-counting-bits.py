f37c579bae131a88d0e651a7d6b2a3541027cc9c
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-15 16:13:36 -0700

    Time: 3 ms (95.05%), Space: 18.6 MB (33.94%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,10 +1,11 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        
         dp = [0] * (n + 1)
-        dp[0] = 0
         bit_mask = 1
         for i in range(1, n + 1):
             if bit_mask * 2 == i:
                 bit_mask = i
+            
             dp[i] = 1 + dp[i - bit_mask]
         return dp
