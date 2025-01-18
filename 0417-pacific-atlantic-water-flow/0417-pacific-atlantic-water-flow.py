032d09652ed75d5b0d979c7db23e8226443ba5aa
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-18 13:25:06 -0700

    Time: 41 ms (37.38%), Space: 19.5 MB (27.16%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,37 +2,34 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows = len(heights)
         cols = len(heights[0])
-        directions = [(1,0), (0,1), (-1, 0), (0, -1)]
-        pac, atl = set(), set()
+        at_reach = set()
+        pac_reach = set()
 
-        def valid(r,c):
-            0 <= r < rows and 0 <= c < cols
-        
-        def dfs(r, c, visited, prevHeight):
-            if ((r,c) in visited or r < 0 or c < 0 or r == rows or c == cols or heights[r][c]< prevHeight):
+        def valid(r, c):
+            return (0 <= r < rows) and (0 <= c < cols)
+
+        def dfs(r, c, visited, lastHeight):
+            if not valid(r, c) or (heights[r][c] < lastHeight) or (r,c) in visited:
                 return
-            
+
             visited.add((r,c))
             dfs(r + 1, c, visited, heights[r][c])
             dfs(r - 1, c, visited, heights[r][c])
             dfs(r, c + 1, visited, heights[r][c])
             dfs(r, c - 1, visited, heights[r][c])
+            return
+        
         for c in range(cols):
-            dfs(0, c, pac, heights[0][c])
-            dfs(rows - 1, c, atl, heights[rows - 1][c])
+            dfs(0, c, pac_reach, heights[0][c])
+            dfs(rows - 1, c, at_reach, heights[rows - 1][c])
         
         for r in range(rows):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, cols - 1, atl, heights[r][cols - 1])
-        
+            dfs(r, 0, pac_reach, heights[r][0])
+            dfs(r, cols - 1, at_reach, heights[r][cols - 1])
+
         res = []
         for r in range(rows):
             for c in range(cols):
-                if (r,c) in pac and (r,c) in atl:
+                if (r,c) in at_reach and (r, c) in pac_reach:
                     res.append([r,c])
         return res
-
-            
-
-            
-            
