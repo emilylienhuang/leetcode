9e3f2b8eeea37acac559466ad66b9cea20e10b94
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-26 17:14:58 -0700

    Time: 43 ms (61.92%), Space: 18.2 MB (13.05%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -12,18 +12,16 @@ class Solution:
                 return False
             if not subRoot:
                 return True
-            
             if self.sameTree(root, subRoot):
                 return True
-            
             return dfs(root.left, subRoot) or dfs(root.right, subRoot)
+        
         return dfs(root, subRoot)
-    
     def sameTree(self, root, subRoot):
         if not root and not subRoot:
             return True
         if not root or not subRoot:
             return False
-        if root and subRoot and root.val == subRoot.val:
-            return self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right)
-        return False
+        if root.val != subRoot.val:
+            return False
+        return self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right)
