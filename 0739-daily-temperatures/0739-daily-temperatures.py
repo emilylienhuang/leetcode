4a79e1fb3f8f18cfec0119c8843ae50a9598591c
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-30 11:45:37 -0700

    Time: 130 ms (32.03%), Space: 33 MB (39.37%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,14 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n =  len(temperatures)
-        ans = [0] * n
-
+        ans = [0] * len(temperatures)
         stack = []
         for i, temp in enumerate(temperatures):
+            
             while stack and stack[-1][1] < temp:
-                # we found a warmer day
                 old_i, old_temp = stack.pop()
                 ans[old_i] = i - old_i
-            if not stack or stack[-1][1] >= temp:
-                stack.append([i, temp])
+            stack.append((i, temp))
         return ans
+        
+            
