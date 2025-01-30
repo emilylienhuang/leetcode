303710239a78359a8208739bfdbbd9f32d189362
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-01-29 19:38:23 -0700

    Time: 0 ms (100%), Space: 12.9 MB (80.44%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        d = collections.defaultdict(int)
+        for i, num in enumerate(nums):
+            if (target - num) in d:
+                return [i, d[target - num]]
+            d[num] = i
+        
+            
