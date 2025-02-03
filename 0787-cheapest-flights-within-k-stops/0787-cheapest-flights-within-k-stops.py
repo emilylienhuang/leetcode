e1077ee30f1010221597e48bf96900366bbe0384
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-02-03 15:34:46 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        
+        # use bellman-ford algorithm
+
+        prices = [float('inf')] * n
+        prices[src] = 0
+
+        for _ in range(k + 1):
+            tempArr = prices[:]
+            for s, d, p in flights:
+                if prices[s] == float('inf'):
+                    continue
+                newPrice = p + prices[s]
+                if newPrice < prices[d]:
+                    tempArr[d] = newPrice
+            prices = tempArr
+        print(prices)
+        return -1 if prices[dst] == float('inf') else prices[dst]
