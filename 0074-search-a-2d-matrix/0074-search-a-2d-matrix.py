e7395d86b06e5a34aa6de0f59fc50d30631ba401
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-02-05 12:27:21 -0700

    Time: 0 ms (100%), Space: 18.2 MB (37.81%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -6,25 +6,25 @@ class Solution:
         #rows increasing
         top = 0
         bottom = ROWS - 1
-        while top < bottom:
+        while top <= bottom:
             mid = (top + bottom) // 2
             if matrix[mid][-1] == target:
-                top = mid
-                break
+                return True
             elif matrix[mid][-1] < target:
                 top = mid + 1
             else:
                 bottom = mid - 1
         
-        if top > bottom:
+        if top >= ROWS:
             return False
         
         left, right = 0, COLS - 1
+        row = top
         while left <= right:
             mid = (left + right) // 2
-            if matrix[top][mid] == target:
+            if matrix[row][mid] == target:
                 return True
-            elif matrix[top][mid] < target:
+            elif matrix[row][mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
