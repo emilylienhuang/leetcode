f39cbc143fd3e01aea67adb52e40ad917494a59d
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-02-28 09:46:27 -0700

    Time: 0 ms (100%), Space: 17.7 MB (49.74%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,14 +1,16 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         pointer = len(digits) - 1
-        while digits[pointer] == 9 and pointer >= 0:
-            #find the position to insert the 1
+        while pointer >= 0 and digits[pointer] == 9:
+            # we are finding insertion location
             digits[pointer] = 0
             pointer -= 1
         
-        if pointer == -1:
+        # insert because we found the position of pointer
+        if pointer < 0:
             digits.insert(0, 1)
         else:
             digits[pointer] += 1
         return digits
+        
 
