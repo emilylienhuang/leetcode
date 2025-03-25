d189c134e1ffdaeb1009ac0001814b81462e27a4
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-03-25 14:50:38 -0600

    Time: 4166 ms (11.48%), Space: 18.2 MB (42.07%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,17 +1,19 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        if n == 1:
-            return 1
-            
+
         perfect_squares = []
-        for i in range(n):
+        for i in range(n + 1):
             if math.isqrt(i) * math.isqrt(i) == i:
                 perfect_squares.append(i)
-        
+
+        if n in perfect_squares:
+            return 1
+
         dp = [float('inf')] * (n + 1)
         dp[0] = 0
         for i in range(n + 1): 
             for num in perfect_squares:
-                dp[i] = min(dp[i], dp[i - num] + 1)
+                dp[i] = min([dp[i], dp[i - num] + 1])
+        print(dp)
         return dp[-1]
             
