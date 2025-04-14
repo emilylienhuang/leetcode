891c402ebdd6e004812d1fc7f3e70867a7679192
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-04-14 10:53:15 -0600

    Time: 193 ms (66.96%), Space: 18.2 MB (62.38%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,10 +1,18 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        if amount == 0:  
+            return 1
+        
+        if amount < min(coins):
+            return 0
+        
+        # the dp array storing the values
         dp = [0] * (amount + 1)
         dp[0] = 1
 
         for coin in coins:
-            for i in range(coin, amount + 1):
-                change = i - coin
-                dp[i] += dp[change]
+            for a in range(1, amount + 1):
+                change = a - coin
+                if change >= 0:
+                    dp[a] += dp[change]
         return dp[-1]
