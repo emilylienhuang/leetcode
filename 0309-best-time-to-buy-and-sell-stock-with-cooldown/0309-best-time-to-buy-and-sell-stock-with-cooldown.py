6d9ecfb00728cc3704e0976fd4ad447e8a35c1dc
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-04-14 10:37:24 -0600

    Time: 5 ms (55.43%), Space: 19.2 MB (19.23%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,23 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = {} # key: (idx, buying), val: max profit
+        dp = {}
 
+        # buying == boolean, idx == where at in list
         def dfs(idx, buying):
-            # if we have over recursed
+
+            # return if we are out of bounds
             if idx >= len(prices):
                 return 0
-                
+            
             if (idx, buying) in dp:
                 return dp[(idx, buying)]
             
             cooldown = dfs(idx + 1, buying)
+
             if buying:
                 buy = dfs(idx + 1, not buying) - prices[idx]
-                dp[(idx, buying)] = max(buy, cooldown)
+                dp[(idx, buying)] =  max(buy, cooldown)
             else:
+                # sell
                 sell = dfs(idx + 2, not buying) + prices[idx]
-                dp[(idx, buying)] = max(sell, cooldown)
-            return dp[(idx,buying)]
-        return dfs(0, True)
-
-            
+                dp[(idx, buying)] =  max(sell, cooldown)
+            return dp[(idx, buying)]
+        
+        ans = dfs(0, True)
+        return ans
+        
