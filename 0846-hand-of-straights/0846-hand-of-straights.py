cd50e419100e66c44105b79be7698b00d989aa4d
Emily Ng <97980289+emilylienhuang@users.noreply.github.com>
2025-04-15 17:30:57 -0600

    Time: 38 ms (41.2%), Space: 19 MB (74.52%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -2,27 +2,13 @@ class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) % groupSize:
             return False
-        def filledGroup(arr):
-            return (len(arr) == groupSize)
-        hand.sort()
-        q = deque(hand)
-        res = [[] for _ in range(groupSize)]
-        group_idx = 0
-        default = 0
-        while q: 
-            num = q.popleft()
-            while res[group_idx] and num <= res[group_idx][-1] and group_idx < len(res):
-                group_idx += 1
-            
-            res[group_idx].append(num)
-            if filledGroup(res[group_idx]):
-                default = group_idx + 1
-            group_idx = default
         
-        for group in res:
-            if len(group) != groupSize:
-                return False
+        num_count = Counter(hand)
+        hand.sort()
+        for num in hand:
+            if num_count[num]:
+                for i in range(num, num + groupSize):
+                    if not num_count[i]:
+                        return False
+                    num_count[i] -= 1
         return True
-            
-
-
